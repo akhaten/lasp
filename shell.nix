668bc83bcf508847e31e13a998d9bb19fb203424
akhaten <khafifjessy.github@gmail.com>
2023-02-22 16:31:54 +0100

    shell.nix file added

---
+++ shell.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {}, ... }:
+
+pkgs.mkShell {
+
+    buildInputs = with pkgs; [
+
+        python310
+        python310Packages.numpy
+        python310Packages.scipy
+        python310Packages.scikitimage
+        python310Packages.matplotlib
+
+
+        # Package for Jupyter
+        python310Packages.ipywidgets
+        python310Packages.ipykernel
+        python310Packages.ipympl
+
+        
+
+    ];
+
+}
