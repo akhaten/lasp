7bfce8c14c50b8acfdfab20517909cfe78a417ef
akhaten <khafifjessy.github@gmail.com>
2023-09-18 13:12:55 +0200

    norm deleted

--- lasp/norms/matrix.py
+++
@@ -1,5 +0,0 @@
-import numpy
-import scipy.linalg
-
-def norm2(matrix: numpy.ndarray) -> float:
-    return numpy.max(scipy.linalg.svdvals(matrix))
--- lasp/norms/vector.py
+++
@@ -1,6 +0,0 @@
-import numpy
-
-import lasp.metrics
-
-def euclidean(signal: numpy.ndarray) -> float:
-    return numpy.sqrt(lasp.utils.metrics.power(signal))   
